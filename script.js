0419a02c3ab025278f1fbe81b1b138c93fa722a2
karan1206 <64606159+karanjoshi1206@users.noreply.github.com>
2021-01-26 09:04:58 +0530

    styling changes

--- script.js
+++ script.js
@@ -33,39 +33,39 @@ $(document).ready(function () {
 })
 
 
-function theme() {
-    let home = document.getElementById("home")
-    let nav = document.getElementById('nav')
-    let content = document.getElementById("content")
-    let rules = document.getElementById('rules')
-    let more = document.querySelector(".more_content")
-    let timeline = document.getElementById('timeline')
-    let faq = document.getElementById('faq')
-    let faq_heading = document.getElementById('faq_heading')
-    let contact = document.getElementById('contact')
-    let main_title = document.querySelectorAll(".main_title")
-    let imp = document.querySelector(".imp")
-    let lines = document.querySelectorAll(".line")
-    home.classList.toggle("theme")
-    nav.classList.toggle('theme')
-    content.classList.toggle('theme')
-    rules.classList.toggle("theme")
-    more.classList.toggle('theme')
-    timeline.classList.toggle("theme")
-    faq.classList.toggle("theme")
-    faq_heading.classList.toggle("theme")
-    lines.forEach((line) => {
-        line.classList.toggle("theme")
-    })
-    imp.forEach((imps) => {
-        imps.classList.toggle("theme")
-    })
-    imp.classList.toggle("theme")
-    main_title.forEach((title) => {
-        title.classList.toggle("theme")
-    })
+// function theme() {
+//     let home = document.getElementById("home")
+//     let nav = document.getElementById('nav')
+//     let content = document.getElementById("content")
+//     let rules = document.getElementById('rules')
+//     let more = document.querySelector(".more_content")
+//     let timeline = document.getElementById('timeline')
+//     let faq = document.getElementById('faq')
+//     let faq_heading = document.getElementById('faq_heading')
+//     let contact = document.getElementById('contact')
+//     let main_title = document.querySelectorAll(".main_title")
+//     let imp = document.querySelector(".imp")
+//     let lines = document.querySelectorAll(".line")
+//     home.classList.toggle("theme")
+//     nav.classList.toggle('theme')
+//     content.classList.toggle('theme')
+//     rules.classList.toggle("theme")
+//     more.classList.toggle('theme')
+//     timeline.classList.toggle("theme")
+//     faq.classList.toggle("theme")
+//     faq_heading.classList.toggle("theme")
+//     lines.forEach((line) => {
+//         line.classList.toggle("theme")
+//     })
+//     imp.forEach((imps) => {
+//         imps.classList.toggle("theme")
+//     })
+//     imp.classList.toggle("theme")
+//     main_title.forEach((title) => {
+//         title.classList.toggle("theme")
+//     })
 
-}
+// }
 
 $(document).ready(function () {
     $(".secondary_list_items").click(function () {
